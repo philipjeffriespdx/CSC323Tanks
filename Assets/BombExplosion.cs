961913becd16f5cb0a26eaa50d38723c711c1d89
Your Name <you@example.com>
2016-10-06 23:32:15 -0700

    First commit

---
+++ Assets/BombExplosion.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class BombExplosion : MonoBehaviour
+{
+	public LayerMask m_TankMask;
+	public ParticleSystem m_BombParticles;       
+	public AudioSource m_BombAudio;              
+	public float m_MaxDamage = 100f;                  
+	public float m_ExplosionForce = 1000f;            
+	public float m_ExplosionRadius = 5f;  
+	public float damage;
+
+
+	private void Start()
+	{
+	}
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		// Find all the tanks in an area around the shell and damage them.
+
+		Collider[] colliders = Physics.OverlapSphere (transform.position, m_ExplosionRadius, m_TankMask);
+
+		for (int i = 0; i < colliders.Length; i++) 
+		{
+			Rigidbody targetRigidbody = colliders [i].GetComponent<Rigidbody> ();
+
+			if (!targetRigidbody) 
+			{
+				continue;
+			}
+
+			targetRigidbody.AddExplosionForce (m_ExplosionForce, transform.position, m_ExplosionRadius);
+
+			TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth> ();
+
+			if (!targetHealth)
+				continue;
+
+			targetHealth.TakeDamage (damage);
+		}
+
+		m_BombParticles.transform.parent = null;
+
+		m_BombParticles.Play ();
+
+		m_BombAudio.Play ();
+
+		Destroy (m_BombParticles.gameObject, m_BombParticles.duration);
+
+		Destroy (gameObject);
+	}
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# CSC323Tanks
-# CSCTanks
-# CSC323Tanks
